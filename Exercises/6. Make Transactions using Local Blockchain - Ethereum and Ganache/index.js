0ec783bb1ac400e5b0e6323e3d5d6c89c1057834
Harsh <hjain5164@gmail.com>
2020-05-19 21:58:47 +0530

    Done some Minor Changes

--- Exercises/6. Make Transactions using Local Blockchain - Ethereum and Ganache/index.js
+++ Exercises/6. Make Transactions using Local Blockchain - Ethereum and Ganache/index.js
@@ -12,8 +12,8 @@ var web3 = new Web3("HTTP://127.0.0.1:7545");
 web3.eth.getAccounts().then((accounts) => console.log(accounts));
 
 // -- Step 2: Set the sending and receiving addresses for the transaction.
-var sendingAddress = "0xe275A3B036EA4F6F1cE8a6066710fE1e19B52977";
-var receivingAddress = "0x14B83FB3506af0b37De61fc2FCb696Eb83afCc4B";
+var sendingAddress = "0x8Ac9C90A2078DfD35E0849E54A25aCedFD508463";
+var receivingAddress = "0x8F9C7853622Dc0f5AFE3b9030F7f497Dc4dC61A7";
 
 // -- Step 3: Check the balances of each address
 web3.eth.getBalance(sendingAddress).then(console.log);
@@ -28,11 +28,11 @@ function ascii_2_0xhex(num) {
 }
 // -- Step 4: Set up the transaction using the transaction variables as shown
 var rawTransaction = {
-  nonce: ascii_2_0xhex(2),
+  nonce: ascii_2_0xhex(3),
   to: receivingAddress,
   gasPrice: ascii_2_0xhex(20000000),
   gasLimit: ascii_2_0xhex(30000),
-  value: ascii_2_0xhex(1),
+  value: ascii_2_0xhex(1000000000000000000),
   data: "0x00",
 };
 
@@ -51,7 +51,7 @@ Sign the Transaction
 
 // -- Step 7: Sign the transaction with the Hex value of the private key of the sender
 var privateKeySender =
-  "6c6d951bc3af059006a89cfa09315950e988de13dbdd3f3fc1c08d8c9fcbeeac";
+  "ab9d2c620c7bb445f2daf7d4370f3e3e62a2e0e6e5b18500dbee055b62ad7ee5";
 var privateKeySenderHex = new Buffer(privateKeySender, "hex");
 var transaction = new EthereumTransaction(rawTransaction);
 transaction.sign(privateKeySenderHex);
@@ -64,3 +64,7 @@ Send the transaction to the network
 // -- Step 8: Send the serialized signed transaction to the Ethereum network.
 var serializedTransaction = transaction.serialize();
 web3.eth.sendSignedTransaction(serializedTransaction);
+
+console.log("Transaction Done!!");
+console.log("Gas Price of the Network is:");
+web3.eth.getGasPrice().then(console.log);
